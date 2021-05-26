a42793660e894d418cfac055c41ef126c729a803
dmhd007 <74659265+dmhd007@users.noreply.github.com>
2021-05-26 13:33:01 +0530

    13

---
+++ first 15 programs/ArrayForEach.java
@@ -0,0 +1,8 @@
+public class ArrayForEach {
+    public static void main(String[] args) {
+        int num[]={12,4,5,2};
+        for (int i : num) {
+            System.out.println(i);
+        }
+    }
+}
